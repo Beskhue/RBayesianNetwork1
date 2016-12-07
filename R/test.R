0c51319009a4ac17689515ee51621c4e89e47734
Jordi <jordi.riemens@student.ru.nl>
2016-12-07 19:30:22 +0100

    Initial model specification

--- R/test.R
+++ R/test.R
@@ -4,12 +4,13 @@ library(semPlot)
 d <- readData()
 
 # Lavaan model
-model <- '
-  # regressions
-  num_hrefs ~ n_non_stop_words;
-  num_self_hrefs ~ n_non_stop_words; # number of Mashable links 
-  n_non_stop_words ~ is_weekend;
-'
+#model <- '
+#  # regressions
+#  num_hrefs ~ n_non_stop_words;
+#  num_self_hrefs ~ n_non_stop_words; # number of Mashable links 
+#  n_non_stop_words ~ is_weekend;
+#'
+model <- readLines('model1.lav');
 
 # Run lavaan
 fit <- sem(model, data=d)
@@ -18,4 +19,4 @@ fit <- sem(model, data=d)
 summary(fit, standardized=TRUE)
 
 # Plot diagram
-semPaths(fit, nCharNodes=10, sizeMan=10, sizeLat=16, sizeInt=4)
+#semPaths(fit, nCharNodes=10, sizeMan=10, sizeLat=16, sizeInt=4)
