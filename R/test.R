a3fba2e1931093dd3e841c445f4beff140faed8a
Jordi <jordi.riemens@student.ru.nl>
2016-12-08 23:36:20 +0100

    Update sem parameters and preproc., extract regression coefficients

--- R/test.R
+++ R/test.R
@@ -15,13 +15,19 @@ testData <- d[['test']];
 #  num_self_hrefs ~ n_non_stop_words; # number of Mashable links 
 #  n_non_stop_words ~ is_weekend;
 #'
-model <- readLines('model1.lav');
+model <- readLines('model_all.lav')
 
 # Run lavaan
-fit <- sem(model, data=fitData)
+fit <- sem(model, data=fitData, orthogonal=TRUE, estimator="ULS")
 
 # Output model summary
 summary(fit, standardized=TRUE)
 
 # Plot diagram
 #semPaths(fit, nCharNodes=10, sizeMan=10, sizeLat=16, sizeInt=4)
+
+# Get regression coefficients
+pars <- parTable(fit)
+pars <- pars[pars$op == '~',c(4,13)]
+pars <- data.frame(pars[,2], row.names=pars[,1])
+
