abd555db3d86e2e3556713ae77d45bdee7c79f8e
Jordi <jordi.riemens@student.ru.nl>
2017-01-19 21:23:01 +0100

    Made PC more conservative in independence tests and orientation

--- R/StructureInference.R
+++ R/StructureInference.R
@@ -12,8 +12,11 @@ d <- filter_for_structural_inference(d) # remove all data but var.s we specified
 
 covariance <- cov(d$fit)
 graph <- glasso(covariance, 0.1)
+qg <- qgraph(graph, label.prop=0.9, label.norm='LDA 00', labels=get_display_names_for_structural_inference(), label.scale.equal=FALSE)
 
 correlation <- cov2cor(covariance)
-p <- pc(suffStat = list(C = correlation, n = nrow(d$fit)), p = ncol(d$fit), alpha = 0.99, indepTest = gaussCItest)
+p <- pc(suffStat = list(C = correlation, n = nrow(d$fit)), p = ncol(d$fit), 
+        alpha = 1e-20, conservative = TRUE, solve.confl = TRUE, u2pd = 'relaxed', indepTest = gaussCItest);
 
-qg <- qgraph(graph, label.prop=0.9, label.norm='LDA 00', labels=get_display_names_for_structural_inference(), label.scale.equal=FALSE)
+qg <- qgraph(p, label.prop=0.9, label.norm='LDA 00', labels=get_display_names_for_structural_inference(), label.scale.equal=FALSE)
+
