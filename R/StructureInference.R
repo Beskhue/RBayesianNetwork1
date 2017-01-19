7cf27932a81e69d06a4c4834801e16cc31b1fd14
Beskhue <thomas@kepow.org>
2017-01-19 20:25:33 +0100

    Change significane level for the PC algorithm. Change plotting function.

--- R/StructureInference.R
+++ R/StructureInference.R
@@ -16,5 +16,5 @@ graph <- glasso(covariance, 0.1)
 qg <- qgraph(graph, label.prop=0.9, label.norm='LDA 00', labels=get_display_names_for_structural_inference(), label.scale.equal=FALSE)
 
 correlation <- cov2cor(covariance)
-pc.fit <- pc(suffStat = list(C = correlation, n = nrow(d$fit)), labels = get_display_names_for_structural_inference(), alpha = 0.01, indepTest = gaussCItest)
-plot(pc.fit)
+pc.fit <- pc(suffStat = list(C = correlation, n = nrow(d$fit)), labels = get_display_names_for_structural_inference(), alpha = 0.0000000000000000000000001, indepTest = gaussCItest)
+qgraph(pc.fit)
