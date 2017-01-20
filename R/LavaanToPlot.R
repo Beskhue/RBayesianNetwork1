a6fe82d3f8c3040ca3093e5881a66e9bd4ea4156
Jordi <jordi.riemens@student.ru.nl>
2017-01-20 13:27:36 +0100

    Isolated lavaantoplot in function

--- R/LavaanToPlot.R
+++ R/LavaanToPlot.R
@@ -1,33 +1,34 @@
-complexity <- abbreviate(c('log_average_token_length', 'n_unique_tokens', 'n_non_stop_unique_tokens', 'log_n_non_stop_words'))
-popularity <- abbreviate(c('log_kw_avg_avg', 'log_kw_max_avg', 'log_kw_avg_max', 'log_kw_avg_min'))
-subjectivity <- abbreviate(c('title_subjectivity', 'global_subjectivity', 'abs_title_subjectivity'))
-polarity <- abbreviate(c('title_sentiment_polarity', 'global_sentiment_polarity', 'abs_title_sentiment_polarity'))
-positivity <- abbreviate(c('global_rate_positive_words', 'rate_positive_words', 'avg_positive_polarity'))
-negativity <- abbreviate(c('global_rate_negative_words', 'rate_negative_words', 'avg_negative_polarity'))
-
-latents <- list(complexity, popularity, subjectivity, polarity, positivity, negativity)
-
-other <- abbreviate(c('log_n_tokens_title', 'log_n_tokens_content', 'log_self_reference_avg_sharess', 'log_num_imgs', 'log_num_videos', 'log_num_hrefs', 'log_num_self_hrefs'))
-
-edges <- matrix(nrow = 0, ncol = 2)
-
-for (latent in latents) {
-  for (i in 1:length(latent)) {
-    var1 <- latent[i]
-    if (i < length(latent)) {
-      for (j in (i+1):length(latent)) {
-        var2 <- latent[j]
-        edges <- rbind(edges, c(var1, var2))
+lavaan_to_plot = function() {
+  complexity <- abbreviate(c('log_average_token_length', 'n_unique_tokens', 'n_non_stop_unique_tokens', 'log_n_non_stop_words'))
+  popularity <- abbreviate(c('log_kw_avg_avg', 'log_kw_max_avg', 'log_kw_avg_max', 'log_kw_avg_min'))
+  subjectivity <- abbreviate(c('title_subjectivity', 'global_subjectivity', 'abs_title_subjectivity'))
+  polarity <- abbreviate(c('title_sentiment_polarity', 'global_sentiment_polarity', 'abs_title_sentiment_polarity'))
+  positivity <- abbreviate(c('global_rate_positive_words', 'rate_positive_words', 'avg_positive_polarity'))
+  negativity <- abbreviate(c('global_rate_negative_words', 'rate_negative_words', 'avg_negative_polarity'))
+  
+  latents <- list(complexity, popularity, subjectivity, polarity, positivity, negativity)
+  
+  other <- abbreviate(c('log_n_tokens_title', 'log_n_tokens_content', 'log_self_reference_avg_sharess', 'log_num_imgs', 'log_num_videos', 'log_num_hrefs', 'log_num_self_hrefs'))
+  
+  edges <- matrix(nrow = 0, ncol = 2)
+  
+  for (latent in latents) {
+    for (i in 1:length(latent)) {
+      var1 <- latent[i]
+      if (i < length(latent)) {
+        for (j in (i+1):length(latent)) {
+          var2 <- latent[j]
+          edges <- rbind(edges, c(var1, var2))
+        }
       }
+      
+      edges <- rbind(edges, c(var1, 'shares'))
     }
-    
-    edges <- rbind(edges, c(var1, 'log_shares'))
   }
+  
+  for (var in other) {
+    edges <- rbind(edges, c(var, 'shares'))
+  }
+  
+  return(edges);
 }
-
-for (var in other) {
-  edges <- rbind(edges, c(var, 'log_shares'))
-}
-
-
-qgraph(edges, directed = FALSE, label.prop=0.9, label.norm='LDA 00', label.scale.equal=FALSE)
--- R/StructureInference.R
+++ R/StructureInference.R
@@ -1,6 +1,7 @@
 library(glasso)
 library(pcalg)
 library(qgraph)
+source('LavaanToPlot.R')
 source('ReadData.R')
 source('Preprocess.R')
 
@@ -10,11 +11,15 @@ d <- remove_empty_articles(d)
 d <- preprocess(d)
 d <- filter_for_structural_inference(d) # remove all data but var.s we specified
 
+lavaan.edges <- lavaan_to_plot()
+lavaan.graph <- qgraph(lavaan.edges, directed = FALSE, label.prop=0.9, label.norm='LDA 00', label.scale.equal=FALSE)
+
 covariance <- cov(d$fit)
-graph <- glasso(covariance, 0.1)
-qg <- qgraph(graph, label.prop=0.9, label.norm='LDA 00', labels=abbreviate(get_names_for_structural_inference()), label.scale.equal=FALSE)
+glasso.fit <- glasso(covariance, 0.1)
+glasso.graph <- qgraph(glasso.fit, label.prop=0.9, label.norm='LDA 00', labels=abbreviate(get_names_for_structural_inference()), label.scale.equal=FALSE)
 
 correlation <- cov2cor(covariance)
 pc.fit <- pc(suffStat = list(C = correlation, n = nrow(d$fit)), labels=abbreviate(get_names_for_structural_inference()), 
         alpha = 1e-20, conservative = TRUE, solve.confl = TRUE, u2pd = 'relaxed', indepTest = gaussCItest);
-qg <- qgraph(pc.fit, label.prop=0.9, label.norm='LDA 00', label.scale.equal=FALSE)
+pc.graph <- qgraph(pc.fit, label.prop=0.9, label.norm='LDA 00', label.scale.equal=FALSE)
+
